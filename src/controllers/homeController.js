a1549d618329ff5252c81faa8bc5a03001743071
Jonathan Lacerda <jonathanlacerda210@gmail.com>
2020-05-23 21:20:20 -0300

    Salvando projeto

---
+++ src/controllers/homeController.js
@@ -0,0 +1,9 @@
+const Contato = require('../models/ContatoModel');
+
+exports.index  = async(req, res) => {
+    if (!req.session.user) return res.render('login');
+const contatos = await Contato.buscaContatos();
+   res.render('index', {contatos})
+    return;
+}
+
